b619df25acf3abc228b3a832e454963e29a52415
Paul Villacis <andresvillacis1220@gmail.com>
2023-12-02 13:49:06 -0500

    "Se modifico el envio de datos"

--- procesar_solicitud.php
+++ procesar_solicitud.php
@@ -18,19 +18,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($conn->connect_error) {
         die("Error de conexión: " . $conn->connect_error);
     } else {
-        // Insertar datos en la tabla 'solicitudes'
-        $sql = "INSERT INTO formularios (tit_pro, tit_soli, nom_soli, car_soli) 
-                VALUES ('$titulo_proye', '$titulo_soli', '$nombre_soli', '$cargo')";
+        // Sanitizar los datos (evitar inyección de SQL)
+        $titulo_proye = mysqli_real_escape_string($conn, $titulo_proye);
+        $titulo_soli = mysqli_real_escape_string($conn, $titulo_soli);
+        $nombre_soli = mysqli_real_escape_string($conn, $nombre_soli);
+        $cargo = mysqli_real_escape_string($conn, $cargo);
 
-        if ($conn->query($sql) === TRUE) {
+        // Insertar datos en la tabla 'formularios' con sentencia preparada
+        $sql = "INSERT INTO formularios (tit_pro, tit_soli, nom_soli, car_soli) 
+                VALUES (?, ?, ?, ?)";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssss", $titulo_proye, $titulo_soli, $nombre_soli, $cargo);
+
+        if ($stmt->execute()) {
             echo "Datos insertados correctamente";
         } else {
-            echo "Error al insertar datos: " . $conn->error;
+            echo "Error al insertar datos: " . $stmt->error;
         }
+
+        $stmt->close();
     }
 
     // Cerrar la conexión
     $conn->close();
+
+    // Redirigir al usuario a index.html
     header("Location: index.html");
     exit(); // Asegura que el script se detenga después de la redirección
 }
