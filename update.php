58b0b1fa1f1a27781ca0910d1bc7afffcd5ba7df
David Lopez <dl735894@gmail.com>
2023-12-03 21:43:44 -0500

    modificacion en update

--- update.php
+++ update.php
@@ -1,19 +1,20 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $tit_pro = $_POST["titulo_proye"];
-    $tit_soli = $_POST["titulo_soli"];
-    $num_soli = $_POST["nro_soli"];
-    $nom_sol = $_POST["nombre_soli"];
-    $car_sol = $_POST["cargo"];
-    $des_sol = $_POST["descripcion"];
-    $jus_sol = $_POST["justificacion"];
-    $imp_sol = $_POST["impacto"];
-    $pri_sol = $_POST["prioridad"];
-    $rec_sol = $_POST["recursos_n"];
-    $fec_sol = $_POST["fecha"];
-    $est_sol = $_POST["estado_solicitud"];
-    $res_sol = $_POST["responsable"];
-    $com_sol = $_POST["comentarios"];
+    // Validar y sanar los datos
+    $tit_pro = mysqli_real_escape_string($conn, $_POST["titulo_proye"]);
+    $tit_soli = mysqli_real_escape_string($conn, $_POST["titulo_soli"]);
+    $num_soli = intval($_POST["nro_soli"]);
+    $nom_sol = mysqli_real_escape_string($conn, $_POST["nombre_soli"]);
+    $car_sol = mysqli_real_escape_string($conn, $_POST["cargo"]);
+    $des_sol = mysqli_real_escape_string($conn, $_POST["descripcion"]);
+    $jus_sol = mysqli_real_escape_string($conn, $_POST["justificacion"]);
+    $imp_sol = mysqli_real_escape_string($conn, $_POST["impacto"]);
+    $pri_sol = mysqli_real_escape_string($conn, $_POST["prioridad"]);
+    $rec_sol = mysqli_real_escape_string($conn, $_POST["recursos_n"]);
+    $fec_sol = mysqli_real_escape_string($conn, $_POST["fecha"]);
+    $est_sol = mysqli_real_escape_string($conn, $_POST["estado_solicitud"]);
+    $res_sol = mysqli_real_escape_string($conn, $_POST["responsable"]);
+    $com_sol = mysqli_real_escape_string($conn, $_POST["comentarios"]);
 
     // Incluye el archivo de conexión
     require 'conexion.php';
@@ -43,14 +44,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             WHERE num_for = ?";
 
         $stmt_update = mysqli_prepare($conn, $sql_update);
-        mysqli_stmt_bind_param($stmt_update, "ssisssssssssssi", $tit_pro, $tit_soli, $num_soli, $nom_sol, $car_sol, $des_sol, $jus_sol, $imp_sol, $pri_sol, $rec_sol, $fec_sol, $est_sol, $res_sol, $com_sol, $id);
 
-        if (mysqli_stmt_execute($stmt_update)) {
-            echo "¡Registro actualizado correctamente!";
+        if ($stmt_update) {
+            // Bind parameters
+            mysqli_stmt_bind_param($stmt_update, "ssisssssssssssi", $tit_pro, $tit_soli, $num_soli, $nom_sol, $car_sol, $des_sol, $jus_sol, $imp_sol, $pri_sol, $rec_sol, $fec_sol, $est_sol, $res_sol, $com_sol, $id);
+
+            // Execute statement
+            if (mysqli_stmt_execute($stmt_update)) {
+                echo "¡Registro actualizado correctamente!";
+            } else {
+                echo "Error al actualizar el registro: " . mysqli_stmt_error($stmt_update);
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt_update);
         } else {
-            echo "Error al actualizar el registro: " . mysqli_error($conn);
+            echo "Error al preparar la declaración: " . mysqli_error($conn);
         }
 
+        // Resto del código...
     }
 } else {
     echo "Acceso no autorizado";
